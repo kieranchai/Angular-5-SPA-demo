b9e4c1c205bb8bada757e03ca02aafcb5d28d81f
Kieran Chai <mayork383@gmail.com>
2018-06-27 10:56:43 +0800

    fourth commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from "@angular/router";
+
+import { HomeComponent } from "./home/home.component";
+import { ContactusComponent } from "./contactus/contactus.component";
+import { FoodcourtSummaryComponent } from './foodcourt-summary/foodcourt-summary.component';
+
+const appRoutes : Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home',                component: HomeComponent},
+  {path: 'contactus',           component: ContactusComponent},
+  {path: 'foodcourt-summary',   component: FoodcourtSummaryComponent}
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    RouterModule.forRoot(appRoutes)
   ],
+
+  exports: [
+    RouterModule
+  ],
+
   declarations: []
 })
 export class AppRoutingModule { }
